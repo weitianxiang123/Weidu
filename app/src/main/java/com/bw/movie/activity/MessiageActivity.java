a2f24e92394ec3c2a2f1917484afd073f7be010b
QiuYangM <43641476+QiuYangM@users.noreply.github.com>
2018-11-29 20:53:37 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/res/values-sw1024dp/dimens.xml
    #	app/src/main/res/values-sw1280dp/dimens.xml
    #	app/src/main/res/values-sw1365dp/dimens.xml
    #	app/src/main/res/values-sw384dp/dimens.xml
    #	app/src/main/res/values-sw392dp/dimens.xml
    #	app/src/main/res/values-sw400dp/dimens.xml
    #	app/src/main/res/values-sw410dp/dimens.xml
    #	app/src/main/res/values-sw411dp/dimens.xml
    #	app/src/main/res/values-sw432dp/dimens.xml
    #	app/src/main/res/values-sw480dp/dimens.xml
    #	app/src/main/res/values-sw533dp/dimens.xml
    #	app/src/main/res/values-sw592dp/dimens.xml
    #	app/src/main/res/values-sw600dp/dimens.xml
    #	app/src/main/res/values-sw640dp/dimens.xml
    #	app/src/main/res/values-sw662dp/dimens.xml
    #	app/src/main/res/values-sw720dp/dimens.xml
    #	app/src/main/res/values-sw768dp/dimens.xml
    #	app/src/main/res/values-sw800dp/dimens.xml
    #	app/src/main/res/values-sw811dp/dimens.xml
    #	app/src/main/res/values-sw820dp/dimens.xml
    #	app/src/main/res/values-sw960dp/dimens.xml
    #	app/src/main/res/values-sw961dp/dimens.xml
    #	app/src/main/res/values/dimens.xml

--- app/src/main/java/com/bw/movie/activity/MessiageActivity.java
+++ app/src/main/java/com/bw/movie/activity/MessiageActivity.java
@@ -1,15 +1,53 @@
 package com.bw.movie.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bw.movie.R;
+import com.bw.movie.mvp.base.BaseActivity;
+import com.bw.movie.presenter.MessiageActivityPresenter;
+import com.facebook.drawee.view.SimpleDraweeView;
 
-public class MessiageActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MessiageActivity extends BaseActivity<MessiageActivityPresenter> {
+
+    @BindView(R.id.message_head)
+    SimpleDraweeView mMessageHead;
+    @BindView(R.id.messiage_name)
+    TextView mMessiageName;
+    @BindView(R.id.messiage_sex)
+    TextView mMessiageSex;
+    @BindView(R.id.messiage_data)
+    TextView mMessiageData;
+    @BindView(R.id.messiage_mobile)
+    TextView mMessiageMobile;
+    @BindView(R.id.messiage_emil)
+    TextView mMessiageEmil;
+    @BindView(R.id.user_exit)
+    TextView exitTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messiage);
+    public Class<MessiageActivityPresenter> getClassDelegate() {
+        return MessiageActivityPresenter.class;
     }
+
+    @Override
+    public void initView() {
+        super.initView();
+        delegate.initView(mMessageHead,mMessiageName,mMessiageSex,mMessiageData,mMessiageMobile,mMessiageEmil,exitTextView);
+    }
+    @OnClick({R.id.user_exit})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.user_exit:
+                delegate.exitUser();
+                break;
+        }
+    }
+
 }
